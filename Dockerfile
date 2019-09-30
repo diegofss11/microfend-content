97dc10eb6751ed054ae5383cab7d0fae61497b4e
Diego <diegofss11@gmail.com>
2019-09-30 13:54:20 +1000

    Fix clashing space

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:10.15.0-alpine
-
-COPY package.json .
-RUN npm install
-COPY . .
-
-RUN npm run build
-
-CMD PORT=$PORT npm run start
--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-ReactDOM.hydrate(<App />, document.getElementById('root'));
+ReactDOM.hydrate(<App />, document.getElementById('content-root'));
